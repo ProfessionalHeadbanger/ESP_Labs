bec849fedf1b8667e44e1c28104eaad0da99107d
Vsevolod <45267974+ProfessionalHeadbanger@users.noreply.github.com>
2024-04-29 19:23:36 +0300

    Add files via upload

--- Lab2/main.c
+++ Lab2/main.c
@@ -1,10 +1,9 @@
-
 #include <avr/io.h>
 #include <util/delay.h>
 #include <stdbool.h>
 #include <avr/interrupt.h>
 
-// Глобальные переменные для хранения состояний светодиодов
+
 int led_state1 = 0;
 int led_state2 = 0;
 int led_state3 = 0;
@@ -12,68 +11,101 @@ int led_state3 = 0;
 bool flag1 = false;
 bool flag2 = false;
 
-// Функция для инициализации внешнего прерывания INT0
 void interrupt_init() 
 {
     GIMSK |= (1 << PCIE_B1);
     GIMSK |= (1 << PCIE_B2);
-    PCMSK_B1 |= (1 << BUTTON1);
-    PCMSK_B2 |= (1 << BUTTON2);
+    PCMSK_B1 |= (1 << B1);
+    PCMSK_B2 |= (1 << B2);
 }
 
-// Обработчик внешнего прерывания на PA6 и PA7
+#if defined(__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
+ISR(PCINT1_vect, ISR_ALIASOF(PCINT0_vect));
 ISR(PCINT0_vect) 
 {
-    _delay_ms(50); // Ждем, чтобы избежать дребезга контактов
-    // Проверяем состояние кнопок и устанавливаем соответствующие значения переменных состояния светодиодов
-    if (!(PIN_B1 & (1 << BUTTON1))) // Если кнопка для светодиода 1 нажата
+    _delay_ms(50);
+
+    if (!(PIN_B1 & (1 << B1))) 
     { 
         if (!flag1) 
         {
             led_state1 = !led_state1;
             flag1 = true;
         }
-        _delay_ms(10); // Ждем, чтобы избежать дребезга контактов
+        _delay_ms(10);
     } 
     else 
     {
         flag1 = false;
     }
-    if (!(PIN_B2 & (1 << BUTTON2))) // Если кнопка для светодиода 2 нажата
+    
+    if (!(PIN_B2 & (1 << B2))) 
     { 
         if (!flag2) 
         {
             led_state2 = !led_state2;
             flag2 = true;
         }
-        _delay_ms(10); // Ждем, чтобы избежать дребезга контактов
+        _delay_ms(10);
     } 
     else 
     {
         flag2 = false;
     }
-    // Светодиод 3 горит только если горят 1 и 2 (логическое И)
+    
     led_state3 = led_state1 && led_state2;
 }
+#else
+ISR(PCINT0_vect) 
+{
+    _delay_ms(50);
 
-int main(void) {
-    // Настройка пинов
-    // Светодиоды на выход
+    if (!(PIN_B1 & (1 << B1))) 
+    { 
+        if (!flag1) 
+        {
+            led_state1 = !led_state1;
+            flag1 = true;
+        }
+        _delay_ms(10);
+    } 
+    else 
+    {
+        flag1 = false;
+    }
+    
+    if (!(PIN_B2 & (1 << B2))) 
+    { 
+        if (!flag2) 
+        {
+            led_state2 = !led_state2;
+            flag2 = true;
+        }
+        _delay_ms(10);
+    } 
+    else 
+    {
+        flag2 = false;
+    }
+    
+    led_state3 = led_state1 && led_state2;
+}
+#endif
+
+int main(void) 
+{
     DDR_L1 |= (1 << LED1);
     DDR_L2 |= (1 << LED2);
-    DDR_L3 |= (1 << LED3);
-    // Кнопки на вход
-    DDR_B1 &= ~(1 << BUTTON1);
-    DDR_B2 &= ~(1 << BUTTON2);
-    // Подтягивающие резисторы на кнопки
-    PORT_B1 |= (1 << BUTTON1);
-    PORT_B2 |= (1 << BUTTON2);
+    DDR_L3 |= (1 << LED3); // Set LED pins as outputs
+    DDR_B1 &= ~(1 << B1); // Set button pins as inputs
+    DDR_B2 &= ~(1 << B2);
 
-    // Инициализация прерывания
+    PORT_B1 |= (1 << B1); // Enable internal pull-up resistors on button pins
+    PORT_B2 |= (1 << B2); // Enable internal pull-up resistors on button pins
+
     interrupt_init();
 
-    // Разрешаем прерывания глобально
-    sei();
+    sei(); // Enable global interrupts
 
     while (1) {
         // Устанавливаем состояния светодиодов
